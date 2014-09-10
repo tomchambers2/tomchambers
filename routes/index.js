93814c7334373dc98e0172713cb069ae3c479fcb
Tom Chambers <tom.chambers@gmail.com>
2014-09-10 13:48:30 +0100

    basic site done

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Tom Chambers' });
+});
+
+router.get('/portfolio', function(req, res) {
+  var db = req.db;
+  var collection = db.get('portfolioSites');
+  collection.find({},{},function(e,docs){
+  	res.render('portfolio', { title: 'Tom Chambers', page: 'portfolio', sites: docs });
+  })
+});
+
+router.get('/contact', function(req, res) {
+  res.render('contact', { title: 'Tom Chambers' });
+});
+
+module.exports = router;
