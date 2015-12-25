e53672ad4b228e2c6e34cfce0815f34822c4f869
Tom Chambers <tom.chambers@gmail.com>
2015-12-25 00:56:25 +0000

    better error checking

--- app.js
+++ app.js
@@ -50,29 +50,15 @@ app.use(function(req, res, next) {
 
 /// error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err,
-            title: 'error'
-        });
-    });
-}
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
-        error: {},
+        error: err,
         title: 'error'
     });
 });
-
 
 module.exports = app;
